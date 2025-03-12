0b0746d38f133b7bac4780f20d0e648673e9cf76
unknown <arvida.python@gmail.com>
2025-03-12 19:52:05 -0300

    first commit

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,30 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+// import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve adicionar "primeiro comentário" na lista', () => {
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('campo-tarefa'), {
+            target: {
+                value: 'primeiro comentário'
+            }
+        });
+        fireEvent.click(screen.getByTestId('btn-cadastrar'))
+        // expect(screen.getByText('primeiro comentário')).toBeInTheDocument()
+        fireEvent.change(screen.getByTestId('campo-tarefa'), {
+            target: {
+                value: 'segundo comentário'
+            }
+        });
+
+        fireEvent.click(screen.getByTestId('btn-cadastrar'));
+        expect(screen.getAllByTestId('campo-tarefa-li')).toHaveLength(2);
+    })
+
 });
